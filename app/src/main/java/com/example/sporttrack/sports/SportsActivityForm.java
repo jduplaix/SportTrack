257cd3120a97be0bd4749c432bae78a6276cc78b
jduplaix <jonathan.duplaix@etudiant.univ-lr.fr>
2021-03-17 13:39:54 +0100

    Feat: gestion back pressed depuis form + refactor appels $sport avec var globale

--- app/src/main/java/com/example/sporttrack/sports/SportsActivityForm.java
+++ app/src/main/java/com/example/sporttrack/sports/SportsActivityForm.java
@@ -30,7 +30,8 @@ public class SportsActivityForm extends MyApplication {
 
         db = getDb();
 
-        // si le form est accédé depuis un clic sur item de la liste, récup du libellé > instanciation du sp de l'item
+        // si le form est accédé depuis un clic sur item de la liste,
+        // récup du libellé du sport > instanciation du sport correspondant à l'item
         Intent incIntent = getIntent();
         incSport = db.sportDao().getSport(incIntent.getStringExtra("spLabel"));
         if (incSport != null) {
@@ -51,16 +52,16 @@ public class SportsActivityForm extends MyApplication {
             sportSave();
         });
 
-        //Suppression du sport identifié dans le formulaire
+        //Suppression du sport
         findViewById(R.id.deleteSport).setOnClickListener(v -> {
             EditText etSpLabel = findViewById(R.id.sportLabel);
-            Sport sp = db.sportDao().getSport(etSpLabel.getText().toString());
-            if (sp == null){
+            sport = db.sportDao().getSport(etSpLabel.getText().toString());
+            if (sport == null){
                 Toast.makeText(this,
                         "Le sport \""+ etSpLabel.getText().toString() + "\" n'existe pas."
                         , Toast.LENGTH_SHORT).show();
             } else {
-                deleteSport(sp);
+                deleteSport();
             }
         });
 
@@ -70,9 +71,17 @@ public class SportsActivityForm extends MyApplication {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        // l'activity list est fermée; réouverture si back pressed
+        Intent intent = new Intent(SportsActivityForm.this, SportsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
     protected void sportSave(){
         /*
-        * Déclenche les contrôles et l'enregistrement de la saisie
+        * Déclenche les contrôles et l'enregistrement de la saisie form
         */
         //récupération des valeurs du form
         //libellé du sport
@@ -84,28 +93,27 @@ public class SportsActivityForm extends MyApplication {
         SwitchCompat swSportTime = (SwitchCompat) findViewById(R.id.sportTime);
         int time = swSportTime.isChecked() ? 1 : 0;
         //hydratation d'un nouveau sport avec la saisie du form
-        Sport sp  = new Sport(etSpLabel.getText().toString(),length,time);
+        sport  = new Sport(etSpLabel.getText().toString(),length,time);
         // Contrôle de saisie
-        String check = integrityCheck(sp);
+        String check = integrityCheck();
         if (check.isEmpty()) {
             // si modif sport de la liste
             if (incSport != null) {
-                sport = sp;
                 upgradeSport();
             } else {
                 // sinon verif si sportLabel existe déjà avant insert
-                Sport checkDup = duplicateCheck(sp);
+                Sport checkDup = duplicateCheck();
                 if (checkDup == null){
-                    insertSport(sp);
+                    insertSport();
                 } else {
                     // si le sport existe déjà alors qu'on veut le créer, snack proposition modif de l'existant en base
                     View snackAnchor = findViewById(R.id.saveSport);
                     Snackbar snackUpdate = Snackbar
-                            .make(snackAnchor, "Le sport \"" + sp.getLabel() + "\" existe déjà.", Snackbar.LENGTH_INDEFINITE)
+                            .make(snackAnchor, "Le sport \"" + sport.getLabel() + "\" existe déjà.", Snackbar.LENGTH_INDEFINITE)
                             .setAction("Modifier", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    updateSport(sp);
+                                    updateSport();
                                 }
                             });
                     snackUpdate.setAnchorView(snackAnchor).show();
@@ -118,7 +126,7 @@ public class SportsActivityForm extends MyApplication {
         }
     }
 
-    protected String integrityCheck(Sport sport){
+    protected String integrityCheck(){
         /* 4 tests menés :
          - le label ne doit pas contenir de saut de ligne
          - le label ne doit pas être vide
@@ -132,33 +140,33 @@ public class SportsActivityForm extends MyApplication {
         int length = sport.getTrackLength();
         String newLine = System.getProperty("line.separator");
 
-        if (label.contains(newLine)){
+        if (sport.getLabel().contains(newLine)){
             checkResult = "Erreur de saisie :\n" +
                     "Le libellé du sport ne doit pas contenir de saut de ligne.";
         }
-        if (label.isEmpty()){
+        if (sport.getLabel().isEmpty()){
             checkResult = checkResult == "" ? "Erreur de saisie :" : checkResult;
             checkResult = checkResult + "\nLe libellé du sport est obligatoire.";
         }
-        if (label.length() >= 80){
+        if (sport.getLabel().length() >= 80){
             checkResult = checkResult == "" ? "Erreur de saisie :" : checkResult;
             checkResult = checkResult + "\nLe libellé du sport est trop long (80 caractères maximum.";
         }
-        if (time + length == 0){
+        if (sport.getTrackTime() + sport.getTrackLength() == 0){
             checkResult = checkResult == "" ? "Erreur de saisie :" : checkResult;
             checkResult = checkResult + "\nLe sport doit au moins pouvoir être suivi sur la distance ou la durée.";
         }
         return checkResult;
     }
 
-    protected Sport duplicateCheck(Sport sport){
+    protected Sport duplicateCheck(){
         // vérification doublon de sport
         // si le sport existe déjà, proposition écrasement avec les données du form actuel
         Sport check = db.sportDao().getSport(sport.getLabel());
         return check;
     }
 
-    protected void insertSport(Sport sport){
+    protected void insertSport(){
         db.sportDao().insert(sport);
         Toast.makeText(this,
                 "Le sport \""+ sport.getLabel() + "\" a bien été enregistré."
@@ -170,7 +178,7 @@ public class SportsActivityForm extends MyApplication {
         finish();
     }
 
-    protected void updateSport(Sport sport){
+    protected void updateSport(){
         try {
             db.sportDao().updateSport(sport);
             Toast.makeText(this,"Le sport \"" + sport.getLabel() + "\" a bien été modifié.",Toast.LENGTH_SHORT).show();
@@ -182,7 +190,7 @@ public class SportsActivityForm extends MyApplication {
         }
     }
 
-    protected void deleteSport(Sport sport){
+    protected void deleteSport(){
         //snack confirmation avant suppression
         View snackAnchor = findViewById(R.id.saveSport);
         Snackbar snackDelete = Snackbar
