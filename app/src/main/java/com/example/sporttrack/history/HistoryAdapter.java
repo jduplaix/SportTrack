66597fffe11d70479a5a26685cef832fe2006bf4
jduplaix <jonathan.duplaix@etudiant.univ-lr.fr>
2021-03-19 18:54:16 +0100

    Feat: affichage histo suivis durée + distance + durée/distance. TODO: upd,del

--- app/src/main/java/com/example/sporttrack/history/HistoryAdapter.java
+++ app/src/main/java/com/example/sporttrack/history/HistoryAdapter.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import com.example.sporttrack.R;
 import com.example.sporttrack.db.TrackedSport;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class HistoryAdapter extends ArrayAdapter<TrackedSport> {
@@ -30,15 +31,46 @@ public class HistoryAdapter extends ArrayAdapter<TrackedSport> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        // récupération de l'id de l'objet trackedSport
-        int histId = getItem(position).getId();
+        // récupération de l'objet trackedSport
+        TrackedSport track = getItem(position);
 
         LayoutInflater inflater = LayoutInflater.from(myContext);
         convertView = inflater.inflate(myResource,parent,false);
 
-        //data binding
+        // instanciation des composants du layout adapter pour binder les data du suivi
         TextView tvSport = convertView.findViewById(R.id.tvHistSport);
-        tvSport.setText(String.valueOf(histId));
+        TextView tvStart = convertView.findViewById(R.id.tvHistStart);
+        TextView tvTime = convertView.findViewById(R.id.tvHistTime);
+        TextView tvDist = convertView.findViewById(R.id.tvHistDist);
+
+        // Définition d'un Date formater
+        SimpleDateFormat myDateTimeFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        // Binding des attr de l'objet trackedTime avec chaque TextView correspondant
+        tvSport.setText(track.getTrackedSport());
+        tvStart.setText(tvStart.getText() + myDateTimeFormat.format(track.getTrackingStart()));
+
+        // Contournement de l'absence d'objet sport pour vérifier ce qui doit être suivi pour le sport
+        // Si pas de durée enregistrée, je considère qu'elle n'est pas suivie pour ce sport
+        // idem pour la distance
+        if (track.getElapsedDays() == 0
+                && track.getElapsedHours() ==0
+                && track.getElapsedMinutes() == 0
+                && track.getElapsedSeconds() == 0) {
+            tvTime.setVisibility(TextView.GONE);
+        } else {
+            tvTime.setText(track.getElapsedDays() + "j, "
+                    + track.getElapsedHours() + "h"
+                    + track.getElapsedMinutes() + "m"
+                    + track.getElapsedSeconds() + "s"
+            );
+        }
+
+        if (track.getTrackedDistance() == 0){
+            tvDist.setVisibility(TextView.GONE);
+        } else {
+            tvDist.setText(track.getTrackedDistance() + "km");
+        }
 
         return convertView;
     }
